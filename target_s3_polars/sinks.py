94f5f67fc051827415f916c8fee82cd4841471a2
sergeyz06 <sergeyz06@yandex.ru>
2025-06-10 14:35:14 +0300

    + S3PolarsTarget, S3PolarsSink

--- target_s3_polars/sinks.py
+++ target_s3_polars/sinks.py
@@ -1,14 +1,48 @@
-""". target sink class, which handles writing streams."""
+"""S3 target sink class, which handles writing streams."""
 
 from __future__ import annotations
 
 from singer_sdk.sinks import BatchSink
 
+import os
+import polars as pl
+import fsspec
+from datetime import datetime 
 
-class Sink(BatchSink):
+
+class S3PolarsSink(BatchSink):
     """. target sink class."""
 
-    max_size = 10000  # Max records to write in one batch
+    def __init__(self, target, stream_name, schema, key_properties):
+        super().__init__(target, stream_name, schema, key_properties)
+
+        self.pl_batches: dict = {}
+
+        os.environ["FSSPEC_S3_ENDPOINT_URL"] = target.config.get("s3_endpoint_url")
+        os.environ["FSSPEC_S3_KEY"] = target.config.get("aws_access_key_id")
+        os.environ["FSSPEC_S3_SECRET"] = target.config.get("aws_secret_access_key")
+
+    # max_size = 10000  # Max records to write in one batch
+
+    @property
+    def max_size(self) -> int:
+        """Get max batch size.
+
+        Returns:
+            Max number of records to batch before `is_full=True`
+
+        .. versionchanged:: 0.36.0
+           This property now takes into account the
+           :attr:`~singer_sdk.Sink.batch_size_rows` attribute and the corresponding
+           ``batch_size_rows`` target setting.
+        """
+        # return (
+        #     self.batch_size_rows
+        #     if self.batch_size_rows is not None
+        #     else self.MAX_SIZE_DEFAULT
+        # )
+
+        return self.config["batch_size"]
 
     def start_batch(self, context: dict) -> None:
         """Start a batch.
@@ -23,6 +57,8 @@ class Sink(BatchSink):
         # ------
         # batch_key = context["batch_id"]
         # context["file_path"] = f"{batch_key}.csv"
+
+        self.pl_batches[context["batch_id"]] = pl.DataFrame()
 
     def process_record(self, record: dict, context: dict) -> None:
         """Process the record.
@@ -39,6 +75,13 @@ class Sink(BatchSink):
         # with open(context["file_path"], "a") as csvfile:
         #     csvfile.write(record)
 
+        self.pl_batches[context["batch_id"]] = pl.concat(
+            [
+                self.pl_batches[context["batch_id"]],
+                pl.DataFrame(record)
+            ]
+        )
+
     def process_batch(self, context: dict) -> None:
         """Write out any prepped records and return once fully written.
 
@@ -49,3 +92,14 @@ class Sink(BatchSink):
         # ------
         # client.upload(context["file_path"])  # Upload file
         # Path(context["file_path"]).unlink()  # Delete local copy
+        
+        file_naming_scheme = self.config["file_naming_scheme"].format(
+            stream=self.stream_name,
+            timestamp=datetime.now().strftime('%Y%m%dT%H%M%S'),
+            format="parquet",
+        )
+
+        output_file = f"s3://{self.config["s3_bucket"]}/{self.config["filepath"]}{file_naming_scheme}"
+
+        with fsspec.open(output_file, "wb") as f:
+            self.pl_batches[context["batch_id"]].write_parquet(f, compression="snappy")
--- target_s3_polars/target.py
+++ target_s3_polars/target.py
@@ -6,40 +6,74 @@ from singer_sdk import typing as th
 from singer_sdk.target_base import Target
 
 from target_s3_polars.sinks import (
-    Sink,
+    S3PolarsSink,
 )
 
 
-class Target(Target):
-    """Sample target for .."""
+class S3PolarsTarget(Target):
+    """Sample target for S3PolarsSink."""
 
     name = "target-s3-polars"
 
     config_jsonschema = th.PropertiesList(
         th.Property(
+            "aws_access_key_id",
+            th.StringType(nullable=False),
+            secret=True,  # Flag config as protected.
+            required=True,
+            title="AWS access key id",
+            description="AWS access key id",
+        ),
+        th.Property(
+            "aws_secret_access_key",
+            th.StringType(nullable=False),
+            secret=True,  # Flag config as protected.
+            required=True,
+            title="AWS secret access key",
+            description="AWS secret access key",
+        ),
+        th.Property(
+            "s3_endpoint_url",
+            th.StringType(nullable=False),
+            required=True,
+            title="S3 endpoint URL",
+            description="S3 endpoint URL",
+        ),
+        th.Property(
+            "s3_bucket",
+            th.StringType(nullable=False),
+            required=True,
+            title="S3 bucket name",
+            description="S3 bucket name",
+        ),
+        th.Property(
             "filepath",
             th.StringType(nullable=False),
+            required=False,
             title="Output File Path",
             description="The path to the target output file",
+            default="target_s3_polars/",
         ),
         th.Property(
             "file_naming_scheme",
             th.StringType(nullable=False),
+            required=False,
             title="File Naming Scheme",
             description="The scheme with which output files will be named",
+            default="{stream}-{timestamp}.{format}",
         ),
         th.Property(
-            "auth_token",
-            th.StringType(nullable=False),
-            secret=True,  # Flag config as protected.
-            required=True,
-            title="Auth Token",
-            description="The path to the target output file",
+            "batch_size",
+            th.IntegerType(nullable=False),
+            required=False,
+            title="Output File batch size",
+            description="Number of records per file",
+            default=10000,
         ),
     ).to_dict()
 
-    default_sink_class = Sink
+    default_sink_class = S3PolarsSink
 
 
 if __name__ == "__main__":
-    Target.cli()
+    S3PolarsTarget.cli()
